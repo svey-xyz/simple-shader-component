d2cc0d9d63736aa42837b0c191c1f442f8beffe8
svey <x@svey.xyz>
2024-11-19 21:37:13 +0000

    Build include types

--- build.ts
+++ build.ts
@@ -1,6 +1,6 @@
 const ESM = await Bun.build({
 	entrypoints: ["./src/core/index.ts", "./src/react/index.tsx"],
-	outdir: "./dist/esm",
+	outdir: "./dist",
 	target: "browser",
 	format: "esm",
 	external: ["react"],
@@ -8,19 +8,19 @@ const ESM = await Bun.build({
 	minify: true
 });
 
-const CJS = await Bun.build({
-	entrypoints: ["./src/core/index.ts", "./src/react/index.tsx"],
-	outdir: "./dist/cjs",
-	target: "browser",
-	format: "cjs",
-	external: ["react"],
-	sourcemap: "linked",
-	minify: true
-});
+// const CJS = await Bun.build({
+// 	entrypoints: ["./src/core/index.ts", "./src/react/index.tsx"],
+// 	outdir: "./dist",
+// 	target: "browser",
+// 	format: "cjs",
+// 	external: ["react"],
+// 	sourcemap: "linked",
+// 	minify: true
+// });
 
-if (!ESM.success || !CJS.success) {
+if (!ESM.success) {
 	console.error("Build failed");
-	for (const message of [...ESM.logs, ...CJS.logs]) {
+	for (const message of [...ESM.logs]) {
 		// Bun will pretty print the message object
 		console.error(message);
 	}
